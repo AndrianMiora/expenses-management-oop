35ce7a9c9ea2f76aadc49b79a5087a69dbfda45c
N0mena <alida.rajaonarivelo@gmail.com>
2025-08-07 20:47:49 +0300

    feat: add refundable expenses

--- src/main/java/expenses/RefundableExpenses.java
+++ src/main/java/expenses/RefundableExpenses.java
@@ -1,4 +1,22 @@
 package expenses;
 
-public class RefundableExpenses {
+import java.time.LocalDate;
+
+public class RefundableExpenses extends Expenses {
+    private boolean refunded;
+
+    public RefundableExpenses(String label, double amount, LocalDate date, boolean refunded) {
+        super(amount, date, label);
+        this.refunded = refunded;
+    }
+
+    public boolean getRefunded(){
+        return refunded;
+    }
+
+    public void refund(boolean refunded){
+        if(this.getRefunded()){
+            this.refunded = true;
+        }
+    }
 }
