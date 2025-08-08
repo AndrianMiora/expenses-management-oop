4a28376b8bd8fe1e1a815cc41b922575936263c1
tsiory <hei.tsiory.234@gmail.com>
2025-08-08 10:10:41 +0300

    Modification du fichier ExpenseDAO

--- src/main/java/expenses/ExpenseDAO.java
+++ src/main/java/expenses/ExpenseDAO.java
@@ -1,4 +1,41 @@
 package expenses;
 
+import java.util.List;
+
 public class ExpenseDAO {
+    List<Expenses> expenses;
+
+    public ExpenseDAO(List<Expenses> expenses) {
+        this.expenses = expenses;
+    }
+
+    public List<RefundableExpenses> getRefundableExpenses() {
+        return expenses.stream()
+                .filter(exp -> exp instanceof RefundableExpenses)
+                .map(exp -> (RefundableExpenses)exp)
+                .filter(exp -> !exp.isRefunded())
+                .toList();
+    }
+
+    public double totalRecurringExpense(){
+        return expenses.stream()
+                .filter(exp -> exp instanceof  RecurringExpenses)
+                .mapToDouble(Expenses::getAmount)
+                .sum();
+    }
+
+    public List<String> listLabel(){
+        return  expenses.stream()
+                .filter(exp -> exp.getAmount() > 100)
+                .map(Expenses::getLabel)
+                .toList();
+    }
+
+
+    public double totalAmount(){
+        return expenses.stream()
+                .mapToDouble(Expenses::getAmount)
+                .sum();
+    }
 }
+
