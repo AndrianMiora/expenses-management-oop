0eddf8c14baf581284b3eacbf40be809f672f324
N0mena <alida.rajaonarivelo@gmail.com>
2025-08-08 10:34:53 +0300

    feat: add  expenses

--- src/main/java/expenses/ExpenseDAO.java
+++ src/main/java/expenses/ExpenseDAO.java
@@ -1,4 +1,28 @@
 package expenses;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ExpenseDAO {
+    private List<RecurringExpenses> recurringExpenses ;
+
+    public ExpenseDAO(List<RecurringExpenses> recurringExpenses, List<RefundableExpenses> refundableExpense) {
+        this.recurringExpenses = recurringExpenses;
+        this.refundableExpense = refundableExpense;
+    }
+
+
+    public List<Double> notRefundedYet(List<RefundableExpense> refundableExpense){
+        if(refundableExpense.refund = false ){
+            return refundableExpense.stream()
+                    .collect(Collectors.toList());
+
+        }
+    }
+
+    public double sumOfRecurrentExpense(List<RecurringExpense> recurringExpenses){
+        return recurringExpenses.stream()
+                .reduce((previousExpense, actualExpense) -> previousExpense + actualExpense).toList();
+    }
+
 }
--- src/main/java/expenses/RecurringExpenses.java
+++ src/main/java/expenses/RecurringExpenses.java
@@ -1,4 +1,16 @@
 package expenses;
 
+import java.time.LocalDate;
+
 public class RecurringExpenses {
+    private Recurrence recurrence;
+
+    public RecurringExpenses(String label, double amount, LocalDate date) {
+        super(amount, label, date);
+        this.recurrence = recurrence;
+    }
+    public String toString(String recurrence) {
+        return recurrence;
+    }
+
 }
